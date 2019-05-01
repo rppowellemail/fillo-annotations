b27bc3e671857fc8821a121f40e95d300ac876a2
Rob Powell <rppowellemail@gmail.com>
2019-04-30 22:03:06 -0700

    Updating README and example tests

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
-# fillo-annotations
-Helpful Java annotations for provider pattern extracting data from spreadsheets using fillo
+# Fillo Annotations
+
+Java annotations to help extract data to class from spreadsheets using the fillo library.
+
+## Simple Fillo Annotations Example
+
+Given a spreadsheet with the following header-column layout:
+
+
+| TestSheetOneColumnOne | TestSheetOneColumnTwo | TestSheetOneColumnThree | TestSheetOneColumnFour BooleanDropdownValues |
+| --------------------- | --------------------- | ----------------------- |:--------------------------------------------:|
+| valueone              | foo                   | 1                       | TRUE                                         |
+| valuetwo              | bar                   | 2                       | FALSE                                        |
+| ...                   | ...                   | ...                     | ...                                          |
+
+
+The intention is to annotate Java class public instance variables and extract row entries.
+
+The following example shows annotating the class and extracting the spreadsheet:
+
+    public class FilloAnnotationsTestSheetOne {
+        @FilloColumn
+        public String TestSheetOneColumnOne;
+    
+        @FilloColumn
+        public String TestSheetOneColumnTwo;
+    
+        @FilloColumn
+        public String TestSheetOneColumnThree;
+    
+        @FilloColumn (columnName="TestSheetOneColumnFour BooleanDropdownValues")
+        public String TestSheetOneColumnFour;
+    
+    }
+
+Extracting list of annotated class from Fillo recordset:
+
+    ...
+        Fillo fillo=new Fillo();
+        Connection connection=fillo.getConnection(xlsxFilename);
+        Recordset recordset=connection.executeQuery("Select * From HorizontalData");
+        
+        List<HorizontalDataEntry> entries = FilloAnnotationsFactory.extractClassesFromRecordset(recordset, HorizontalDataEntry.class);
+        
+        recordset.close();
+        connection.close();
+    ...
+
+Iterating over recordset:
+
+    ...
+        Fillo fillo=new Fillo();
+        Connection connection=fillo.getConnection(xlsxFilename);
+        Recordset recordset=connection.executeQuery("Select * From TestSheetOne");
+        while(recordset.next()) {
+            FilloAnnotationsTestSheetOne filloAnnotationsTestSheetOne = (FilloAnnotationsTestSheetOne) FilloAnnotationsfactory.extract(recordset, FilloAnnotationsTestSheetOne.class);
+            System.out.println(annotatedFilloTestSheetOne.toString());
+        }
+        recordset.close();
+        connection.close();
+        
+    ...
+
+Values are set in the created class:
+
+    ...
+    FilloAnnotationsTestSheetOne(TestSheetOneColumnOne='valueone', TestSheetOneColumnTwo='foo', TestSheetOneColumnThree='1', TestSheetOneColumnFour='true')
+    FilloAnnotationsTestSheetOne(TestSheetOneColumnOne='valuetwo', TestSheetOneColumnTwo='bar', TestSheetOneColumnThree='2', TestSheetOneColumnFour='false')
+    ...
+
+
+# Dependencies / References
+
+ * Fillo ( https://codoid.com/fillo/ )
+
+
--- src/test/java/com/rppowell/fillo/annotations/TestWorkbook002/FilloAnnotationsTestWorkbook002Example.java
+++ src/test/java/com/rppowell/fillo/annotations/TestWorkbook002/FilloAnnotationsTestWorkbook002Example.java
@@ -6,37 +6,43 @@ import com.codoid.products.fillo.Fillo;
 import com.codoid.products.fillo.Recordset;
 import com.rppowell.fillo.annotations.FilloAnnotationsFactory;
 import com.rppowell.fillo.annotations.FilloColumn;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FilloAnnotationsTestWorkbook002Example {
 
     @Test
     public void testFilloTestSheetTwoVerticalData() throws FilloException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        List<String> expectedValues = Arrays.asList("ValueOne", "ValueTwo", "ValueThree", "ValueFour", "ValueFive", "ValueSix", "ValueSeven", "ValueEight");
+        List<String> actualValues = new ArrayList<>();
         String xlsxFilename = "src/test/resources/TestWorkbook002.xlsx";
         System.out.println("xlsx=" + xlsxFilename);
 
         Fillo fillo=new Fillo();
         Connection connection=fillo.getConnection(xlsxFilename);
         Recordset recordset=connection.executeQuery("Select * From VerticalData");
-        List<VerticalDataEntry> entries;
+        List<VerticalDataEntry> entries = FilloAnnotationsFactory.extractClassesFromRecordset(recordset, VerticalDataEntry.class);
 
-        entries = FilloAnnotationsFactory.extractClassesFromRecordset(recordset, VerticalDataEntry.class);
-
-        List<String> strings = new ArrayList<>();
         for (int i = 0; i < entries.size(); i++) {
-            strings.add(entries.get(i).toString());
+            actualValues.add(entries.get(i).toString());
         }
         recordset.close();
         connection.close();
-        System.out.println(strings);
+        System.out.println(actualValues);
+        Assert.assertEquals(actualValues, expectedValues, "TestSheetTwo VerticalData matches");
     }
 
     @Test
     public void testFilloTestSheetTwoHorizontalData() throws FilloException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        List<String> expectedValues = Arrays.asList(
+                "[ValueOneOne, ValueOneTwo, ValueOneThree, ValueOneFour, ValueOneFive, ValueOneSix]",
+                "[ValueTwoOne, ValueTwoTwo, ValueTwoThree, ValueTwoFour, ValueTwoFive, ValueTwoSix]"
+        );
         String xlsxFilename = "src/test/resources/TestWorkbook002.xlsx";
         System.out.println("xlsx=" + xlsxFilename);
 
@@ -47,12 +53,13 @@ public class FilloAnnotationsTestWorkbook002Example {
 
         entries = FilloAnnotationsFactory.extractClassesFromRecordset(recordset, HorizontalDataEntry.class);
 
-        List<String> strings = new ArrayList<>();
+        List<String> actualValues = new ArrayList<>();
         for (int i = 0; i < entries.size(); i++) {
-            strings.add(entries.get(i).toString());
+            actualValues.add(entries.get(i).toString());
         }
         recordset.close();
         connection.close();
-        System.out.println(strings);
+        System.out.println(actualValues);
+        Assert.assertEquals(actualValues, expectedValues, "TestSheetTwo HorizontalData matches");
     }
 }
